c18aff89ae492c2c912b7aa4795ebb5108053ac2
SoulSkunk <kylian.broccolichi@mediaschool.me>
2023-11-24 11:35:49 +0100

    import modif

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ import { Menu } from "antd";
 //import fichier css
 import "./App.css";
 //import des components
-import Inscription from "./components/Inscription";
-import Connection from "./components/Connection";
-import User from "./components/User";
-import Home from "./components/Home";
-import Annonce from "./components/Annonce";
+import Inscription from "./components/Inscription.jsx";
+import Connection from "./components/Connection.jsx";
+import User from "./components/User.jsx";
+import Home from "./components/Home.jsx";
+import Annonce from "./components/Annonce.jsx";
 
 const App = () => {
   return (
